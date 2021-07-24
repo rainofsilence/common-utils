e1bf63053c4c2a7e536553c806cc5367bdade3e0
Lindsey.Cheung <822466858@qq.com>
2021-07-24 23:04:42 +0800

    fix(pom): remove useless

--- src/main/java/com/lindsey/coutil/sql/SqlBatchBuildUtil.java
+++ src/main/java/com/lindsey/coutil/sql/SqlBatchBuildUtil.java
@@ -2,6 +2,7 @@ package com.lindsey.coutil.sql;
 
 
 import com.lindsey.coutil.sql.dto.OperationResultDTO;
+import com.lindsey.coutil.sql.enums.OptTypeEnum;
 import com.lindsey.coutil.sql.opt.DelOperation;
 import com.lindsey.coutil.sql.opt.InsOperation;
 import com.lindsey.coutil.sql.opt.Operation;
@@ -25,26 +26,6 @@ public class SqlBatchBuildUtil {
 //    private static final Logger logger = LoggerFactory.getLogger(SqlBatchBuildUtil.class);
 
     private static final String FILE_SUFFIX_CSV_UP = "CSV";
-
-    private static final String OPT_TYPE_INSERT = "INS";
-    private static final String OPT_TYPE_UPDATE = "UPD";
-    private static final String OPT_TYPE_DELETE = "DEL";
-
-    private static final String COL_TYPE_STRING = "S";
-    private static final String COL_TYPE_NUMBER = "N";
-    private static final String COL_TYPE_DATETIME = "DT";
-
-    /**
-     * 主键自增
-     */
-    private static final String KEY_TYPE_AUTO = "AI";
-    /**
-     * 雪花
-     */
-    private static final String KEY_TYPE_SNOWFLAKES = "SF";
-    private static final String KEY_TYPE_STRING = "S";
-    private static final String KEY_TYPE_NUMBER = "N";
-
 
     /**
      * @param dataFilePath String 源数据文件路径
@@ -90,30 +71,24 @@ public class SqlBatchBuildUtil {
                     continue;
                 }
                 String optType = items[0].toUpperCase();
-                switch (optType) {
-                    case OPT_TYPE_INSERT:
-                        Operation ins = new InsOperation();
-                        OperationResultDTO iOrDTO = ins.buildSql(items, lineNumber);
-                        if (iOrDTO.getErrorLineNumber() != null)
-                            errorLineNumbers.add(String.valueOf(iOrDTO.getErrorLineNumber()));
-                        resultSql.append(iOrDTO.getSql());
-                        break;
-                    case OPT_TYPE_UPDATE:
-                        Operation upd = new UpdOperation();
-                        OperationResultDTO uOrDTO = upd.buildSql(items, lineNumber);
-                        if (uOrDTO.getErrorLineNumber() != null)
-                            errorLineNumbers.add(String.valueOf(uOrDTO.getErrorLineNumber()));
-                        resultSql.append(uOrDTO.getSql());
-                        break;
-                    case OPT_TYPE_DELETE:
-                        Operation del = new DelOperation();
-                        OperationResultDTO dOrDTO = del.buildSql(items, lineNumber);
-                        if (dOrDTO.getErrorLineNumber() != null)
-                            errorLineNumbers.add(String.valueOf(dOrDTO.getErrorLineNumber()));
-                        resultSql.append(dOrDTO.getSql());
-                        break;
-                    default:
-                        break;
+                if (OptTypeEnum.INSERT.getCode().equals(optType)) {
+                    Operation ins = new InsOperation();
+                    OperationResultDTO iOrDTO = ins.buildSql(items, lineNumber);
+                    if (iOrDTO.getErrorLineNumber() != null)
+                        errorLineNumbers.add(String.valueOf(iOrDTO.getErrorLineNumber()));
+                    resultSql.append(iOrDTO.getSql());
+                } else if (OptTypeEnum.UPDATE.getCode().equals(optType)) {
+                    Operation upd = new UpdOperation();
+                    OperationResultDTO uOrDTO = upd.buildSql(items, lineNumber);
+                    if (uOrDTO.getErrorLineNumber() != null)
+                        errorLineNumbers.add(String.valueOf(uOrDTO.getErrorLineNumber()));
+                    resultSql.append(uOrDTO.getSql());
+                } else if (OptTypeEnum.DELETE.getCode().equals(optType)) {
+                    Operation del = new DelOperation();
+                    OperationResultDTO dOrDTO = del.buildSql(items, lineNumber);
+                    if (dOrDTO.getErrorLineNumber() != null)
+                        errorLineNumbers.add(String.valueOf(dOrDTO.getErrorLineNumber()));
+                    resultSql.append(dOrDTO.getSql());
                 }
             }
             // SQL结果写入文件
@@ -135,6 +110,11 @@ public class SqlBatchBuildUtil {
         return BuildingResult.success();
     }
 
+    /**
+     * <p>测试用,后续会删除
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SqlBatchBuildUtil.building("src/main/resources/data-demo.CSV", "src/main/resources/result.sql");
     }
